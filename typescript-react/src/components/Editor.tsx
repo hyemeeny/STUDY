50a2455f5c1d0c014e17b81eecf50cca08282d7c
jihyemin <hyemeeeny@gmail.com>
2024-09-21 17:46:12 +0900

    typescript-react Todo

---
+++ typescript-react/src/components/Editor.tsx
@@ -0,0 +1,27 @@
+import { ReactElement, useState } from "react";
+import { TodoDispatchContext, useTodoDispatch } from "../App";
+
+interface Props {
+  // children: ReactElement;
+}
+
+export default function Editor(props: Props) {
+  const dispatch = useTodoDispatch();
+  const [text, setText] = useState("");
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onClickButton = () => {
+    dispatch.onClickAdd(text);
+    setText("");
+  };
+
+  return (
+    <div>
+      <input value={text} onChange={onChangeInput} />
+      <button onClick={onClickButton}>추가</button>
+    </div>
+  );
+}
